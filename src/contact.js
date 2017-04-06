2f2b9a7a1c307bf149a79fccdcb24a5cb7a0a110
sunweiling <317764217@qq.com>
2017-04-06 17:35:45 +0800

    no message

--- src/contact.js
+++ src/contact.js
@@ -1,15 +1,41 @@
 Vue.component('v-contact', {
 	template: `<div v-show="show">
 		<div class="other">
-			
+			<div class="o-bg">
+				<h1 style="margin-top:40px;">{{title}}</h1>
+				<h3>{{info}}</h3>
+				<div class="o-con">
+					<div v-for="(_message, _t) of message">
+						<a :href="_message.href" target="_blank"><img :src="_message.pic"/></a>
+						<div>{{_t}}:{{_message.name}}</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>`,
 	props: ['show'],
 	data: function () {
 		var data = {
-			article : {
-
+			title: "联系我／Contact",
+			info: "扫描或点击图片二维码",
+			message: {
+				"新浪微博": {
+					name: "有一个姑娘在coding",
+					pic: "./img/weibo.jpg",
+					href: "http://weibo.com/u/5750254341?refer_flag=1001030101_&is_hot=1"
+				},
+				"微信公众号":{
+					name: "有一个姑娘在coding",
+					pic: "./img/weixin.jpg",
+					href: "http://mp.weixin.qq.com/mp/homepage?__biz=MjM5MDk5MzE2Mw==&hid=1&sn=c156f12e6c46f78445fd1dbc957d44a3#wechat_redirect"
+				},
+				"github": {
+					name: "sunweiling",
+					pic: "./img/github.jpg",
+					href: "https://github.com/sunweiling"
+				}
 			}
+
 		}
 		return data
 	},
--- src/home.js
+++ src/home.js
@@ -1,18 +1,55 @@
 Vue.component('v-home', {
 	template: `<div v-show="show">
-		<div class="bg" style='background-image: url("img/bg.jpeg");'></div>
+	    <div class="bg" :style='bg_src'></div>
+		<div class="b-title">
+		<transition-group name="list" tag="p">	
+		    <span v-for="item in show_key" v-bind:key="item" class="list-item">
+		      {{ item }}
+		    </span>
+	  	</transition-group>
+	  	<transition-group name="list-after" tag="p">	
+		    <span v-for="item in show_key_last" v-bind:key="item" class="list-item2">
+		      {{ item }}
+		    </span>
+	  	</transition-group>
+	  	</div>
 		<div class="cover"></div>
 	</div>`,
 	props: ['show'],
 	data: function () {
 		var data = {
-			bgs:{
-
-			}
+			check: -1,
+			check_last: -1,
+			bg_src: "background-image: url('img/bg.jpeg')",
+			keys: ["早睡早起","常做运动"],
+			keys_last: ["多","与","异","性","交","朋","友"],
+			show_key: [],
+			show_key_last: [],
+			timer_keys_last: null,
+			timer_keys: null
 		}
 		return data
 	},
 	methods:{
-		
+		show_keys: function(){
+			this.check ++;
+			if(this.check >= 2){
+				clearTimeout(this.timer_keys);
+				this.check_last = -1;
+				this.show_keys_last();
+				return;
+			}
+			this.show_key.push(this.keys[this.check]);
+			this.timer_keys = setTimeout(this.show_keys, 1000);
+		},
+		show_keys_last: function(){
+			this.check_last ++;
+			if(this.check_last >= 7){
+				clearTimeout(this.timer_keys_last);
+				return;
+			}
+			this.show_key_last.push(this.keys_last[this.check_last]);
+			this.timer_keys_last = setTimeout(this.show_keys_last, 500);
+		}
 	}
 });
